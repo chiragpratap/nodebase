4d629724c47da92e25b8995eca30ccc8a8bce5ee
Chirag pratap <chiragpratap0009@gmail.com>
2025-12-26 17:12:40 +0530

    Initial commit

---
+++ nodebase/src/app/page.tsx
@@ -0,0 +1,13 @@
+ 
+import { Button } from "@/components/ui/button";
+ const page = () => {
+   
+    return (
+      <div className="min-h-screen min-w-screen flex items-center justify-center">
+        <Button>
+          Click me
+        </Button>
+      </div>
+    );
+};
+export default page;
